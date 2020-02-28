c7a6248f36c7d5ca9c496ca974c58c397fefafb3
ridgedejong <rad47@students.calvin.edu>
2020-02-27 19:24:29 -0500

    Submitting homework 1

--- homework01/schedule.py
+++ homework01/schedule.py
@@ -1,126 +1,54 @@
-import time
-
 
 from csp import *
-from collections import defaultdict
-
-
-def print_solution(result):
-    """A CSP solution printer copied from csp.py."""
-    for h in range(1, 7):
-        print('Class', h)
-        for (var, val) in result.items():
-            if val == h: print('\t', var)
-
 
 def schedule():
-    """Return an instance of the Schedule Puzzle."""
+    # Return a CSP formulation of a scheduling problem
     variables = ['cs108', 'cs112', 'cs212', 'cs214', 'cs262', 'cs344']
-    faculty = 'schuurman adams norman vanderlinden'.split()
-    time = 'mwf800 mwf900 mwf1030 mwf1130'.split()
-    room = 'nh253 sb382'.split()
+
+    # Create the domains for every class variable, with the possible values being time and room
     domains = {}
     for var in variables:
-        domains[var] = [faculty, time, room]
+        domains[var] = [['mwf800', 'nh253'],['mwf900', 'nh253'],['mwf1030', 'nh253'],['mwf1130', 'nh253'],['mwf800', 'sb382'],['mwf900', 'sb382'],['mwf1030', 'sb382'],['mwf1130', 'sb382']]
 
+    # Explicitly assign faculty to specific classes to ensure each one only teaches one class, and to reduce the
+    # total number of possible domains for each variable
+    for i in range(8):
+        domains['cs108'][i].append('schuurman')
+        domains['cs112'][i].append('adams')
+        domains['cs212'][i].append('adams')
+        domains['cs214'][i].append('norman')
+        domains['cs262'][i].append('norman')
+        domains['cs344'][i].append('vanderlinden')
 
-
-    # neighbors = parse_neighbors("""Englishman: Red;
-    #             Spaniard: Dog; Kools: Yellow; Chesterfields: Fox;
-    #             Norwegian: Blue; Winston: Snails; LuckyStrike: OJ;
-    #             Ukranian: Tea; Japanese: Parliaments; Kools: Horse;
-    #             Coffee: Green; Green: Ivory""", variables)
-
+    # The neighbors for each variable are all the other variables (not itself)
     neighbors = {'cs108': ['cs112', 'cs212', 'cs214', 'cs262', 'cs344'], 'cs112': ['cs108', 'cs212', 'cs214', 'cs262', 'cs344'], 'cs212': ['cs108', 'cs112', 'cs214', 'cs262', 'cs344'], 'cs214': ['cs108', 'cs112', 'cs212', 'cs262', 'cs344'], 'cs262': ['cs108', 'cs112', 'cs212', 'cs214', 'cs344'], 'cs344': ['cs108', 'cs112', 'cs212', 'cs214', 'cs262']}
-    # for A in ['cs108', 'cs112', 'cs212', 'cs214', 'cs262', 'cs344']:
-    #     for B in ['cs108', 'cs112', 'cs212', 'cs214', 'cs262', 'cs344']:
-    #         if A != B:
-    #              if B not in neighbors[A]:
-    #                 neighbors[A].append(B)
-    #              if A not in neighbors[B]:
 
-
-
-
-    def schedule_constraint(A, a, B, b, recurse=0):
-        same = (a == b)
-        next_to = abs(a - b) == 1
+    # Define the constraints that the solution must meet
+    def schedule_constraint(A, a, B, b):
 
         # each class cannot be offered more than once (more than one state)
         if A == B:
             return False
         if A != B:
-            if a == b and A[0] == B[0]:
-                return
+            # if both the time and faculty are the same for 2 variables, it is not a solution because faculty cannot
+            # teach 2 classes at the same time
+            if a[0] == b[0] and a[1] == b[1]:
+                return False
+            # if both the time and room are the same for 2 variables, it is not a solution because the same room
+            # cannot be used by 2 classes at the same time
+            if a[0] == b[0] and a[2] == b[2]:
+                return False
+            # if the constraints are satisfied, a solution has been found
+            return True
 
-
-        if a == 'sb382' and b == 'sb382':
-            return False
-       # if domains[A]
-
-        if A == 'Englishman' and B == 'Red':
-            return same
-        if A == 'Spaniard' and B == 'Dog':
-            return same
-        if A == 'Chesterfields' and B == 'Fox':
-            return next_to
-        if A == 'Norwegian' and B == 'Blue':
-            return next_to
-        if A == 'Kools' and B == 'Yellow':
-            return same
-        if A == 'Winston' and B == 'Snails':
-            return same
-        if A == 'LuckyStrike' and B == 'OJ':
-            return same
-        if A == 'Ukranian' and B == 'Tea':
-            return same
-        if A == 'Japanese' and B == 'Parliaments':
-            return same
-        if A == 'Kools' and B == 'Horse':
-            return next_to
-        if A == 'Coffee' and B == 'Green':
-            return same
-        if A == 'Green' and B == 'Ivory':
-            return a - 1 == b
-        if recurse == 0:
-            return schedule_constraint(B, b, A, a, 1)
-        if ((A in Colors and B in Colors) or
-                (A in Pets and B in Pets) or
-                (A in Drinks and B in Drinks) or
-                (A in Countries and B in Countries) or
-                (A in Smokes and B in Smokes)):
-            return not same
-        raise Exception('error')
     return CSP(variables, domains, neighbors, schedule_constraint)
 
-def parse_neighbors(neighbors, variables=None):
-    """Convert a string of the form 'X: Y Z; Y: Z' into a dict mapping
-    regions to neighbors. The syntax is a region name followed by a ':'
-    followed by zero or more region names, followed by ';', repeated for
-    each region name. If you say 'X: Y' you don't need 'Y: X'.
-    >>> parse_neighbors('X: Y Z; Y: Z') == {'Y': ['X', 'Z'], 'X': ['Y', 'Z'], 'Z': ['X', 'Y']}
-    True
-    """
-    dic = defaultdict(list)
-    specs = [spec.split(':') for spec in neighbors.split(';')]
-    for (A, Aneighbors) in specs:
-        A = A.strip()
-        for B in Aneighbors.split():
-            dic[A].append(B)
-            dic[B].append(A)
-    return dic
+# use AIMA search algorithm to solve the problem
+solution = min_conflicts(schedule(), max_steps=10000)
 
-puzzle = schedule()
-
-# result = depth_first_graph_search(puzzle)
-# result = AC3(puzzle)
-result = backtracking_search(puzzle)
-# result = min_conflicts(puzzle, max_steps=1000)
-
-if puzzle.goal_test(puzzle.infer_assignment()):
+# display the solution if one was found
+if solution != None:
     print("Solution:\n")
-    print_solution(result)
+    print(solution)
 else:
-    print("failed...")
-    print(puzzle.curr_domains)
-    puzzle.display(puzzle.infer_assignment())
+    print("Failed: solution returned None")
