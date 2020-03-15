c860dbb47a87063727a507a5e3bcd726d40a7aef
ridgedejong <rad47@students.calvin.edu>
2020-03-15 16:55:20 -0400

    update lab4

--- lab06/lab06_3.py
+++ lab06/lab06_3.py
@@ -1,11 +1,30 @@
-import numpy as np
-# import tensorflow as tf
+
 from keras.datasets import boston_housing
 (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
 
 def print_structures():
-    print(x_train, y_train)
-    print(x_test, y_test)
+    print(
+        'training examples \
+                    \n\tcount: {} \
+                    \n\tdimensions: {} \
+                    \n\tshape: {} \
+                    \n\tdata type: {}\n\n'.format(
+            len(x_train),
+            x_train.ndim,
+            x_train.shape,
+            x_train.dtype
+        ),
+        'testing examples \
+            \n\tcount: {} \
+            \n\tdimensions: {} \
+            \n\tshape: {} \
+            \n\tdata type: {} \n'.format(
+            len(x_test),
+            x_test.ndim,
+            x_test.shape,
+            x_test.dtype,
+        )
+    )
 
 
 print_structures()
